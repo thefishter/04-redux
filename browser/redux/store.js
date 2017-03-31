bb33206c0b662b09bb70ba3e3b37c6c7c6827126
Maria Schreiber <meschreiber3@gmail.com>
2017-03-31 12:56:35 -0400

    added middleware

--- browser/redux/store.js
+++ browser/redux/store.js
@@ -1,7 +1,13 @@
-import {createStore} from 'redux'
+import { createStore
+        , applyMiddleware } from 'redux'
 import reducer from './reducers/root-reducers'
+import { createLogger } from 'redux-logger' 
+
+const logger = createLogger();
+const middleware = applyMiddleware(logger);
+
 
 const store = createStore(reducer,
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), middleware);
 
 export default store;
